213241917a175029d11467dafcc5183198a8599c
CodeRed2019 <31117128+CodeRed2019@users.noreply.github.com>
2019-12-24 21:53:14 -0600

    Update Jail.php

--- src/hoyinm14mc/jail/Jail.php
+++ src/hoyinm14mc/jail/Jail.php
@@ -468,7 +468,6 @@ class Jail extends PluginBase implements JailAPI
         $player->setGamemode(0);
         $player->getInventory()->clearAll();
         $player->getInventory()->setItemInHand(Item::get(278));
-        $player->getInventory()->setItemInHand(Item::get(297));
         $player->sendMessage(
             str_replace("%time%", ($time != -1 ? $time : "infinite"),
                 str_replace("%reason%", $reason, $this->getMessage("jail.success.prisoner"))));
